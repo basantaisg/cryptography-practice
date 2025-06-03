9bc880655348617ededdf7e9926dce78c0e0863f
basantaisg <basantaparajuli.11@gmail.com>
2025-06-03 11:21:14 +0545

    Ecdsa examples handsone

--- index.js
+++ index.js
@@ -9,3 +9,30 @@ console.log(`SHA256: ${hash}`);
 const { keccak256 } = require("js-sha3");
 const keccakHash = keccak256(data);
 console.log(`Keccak256 hash: ${keccakHash}`);
+
+// --------------- Section 2 ----------------
+
+// ECDSA practice with examples
+const EC = require("elliptic").ec;
+const ec = new EC("secp256k1");
+
+// generating a keypair!
+const key = ec.genKeyPair();
+
+const message = "Hey fuck off!";
+const msgHash = crypto.createHash("sha256").update(message).digest();
+
+const signature = key.sign(msgHash);
+
+console.log(
+  `Signature: ${{
+    r: signature.r.toString("hex"),
+    s: signature.s.toString("hex"),
+  }}`
+);
+
+// verifying signature:
+
+const pubKey = key.getPublic();
+const valid = ec.keyFromPublic(pubKey).verify(msgHash, signature);
+console.log(`Signature valid: ${valid}`);
